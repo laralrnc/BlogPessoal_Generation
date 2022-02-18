e784611ab000d6f22a52e71e33214b9064906550
unknown <laralourenco5966@gmail.com>
2022-02-18 13:42:44 -0300

    Atualização BlogPessoal

--- Projeto_BlogPessoal/blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ Projeto_BlogPessoal/blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,10 +2,18 @@ package com.generation.blogpessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +40,45 @@ public class PostagemController {
 		return ResponseEntity.ok(postagemRepository.findAll());
 	}
 	
-
+	//findByID
+	//"/{id}" variavel de caminho
+	//PathVariable anotação de caminho
+	// http://localhost:8080/postagens/3. 3 = "/{id}"
+	@GetMapping("/{id}")
+	public ResponseEntity <Postagem> getById(@PathVariable Long id)
+	{
+		//lambda = (resposta -> retorno)
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	//("/titulo/{titulo}") == ("/nomeColuna/{Valor da variavel}")
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity <List <Postagem>> getByTitulo(@PathVariable String titulo)
+	{
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	//pode usar o mesmo caminho na url, contando que o verbo seja diferente
+	//inserindo um objeto no banco (todas as colunas)
+	@PostMapping
+	public ResponseEntity<Postagem> postPostagem (@Valid @RequestBody Postagem postagem)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).
+				body(postagemRepository.save(postagem));
+	}
+	//metodo de atualização
+	@PutMapping
+	public ResponseEntity<Postagem> putPostagem (@Valid @RequestBody Postagem postagem)
+	{
+		return ResponseEntity.status(HttpStatus.OK).
+				body(postagemRepository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletePostagem(@PathVariable Long id)
+	{
+		postagemRepository.deleteById(id);
+	}
 }
--- Projeto_BlogPessoal/blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ Projeto_BlogPessoal/blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,5 +1,7 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,9 +9,22 @@ import com.generation.blogpessoal.model.Postagem;
 
 //identificando que a interfaace é do tipo repository
 //criando uma herança
-// Postagem = objeto (classe que criei a tabela) e Long = Chave primária da tabela tb_postagem
+// Postagem = objeto (classe que criei a tabela) 
+//e Long = Chave primária da tabela tb_postagem
 //uma interface para cada tabela
+//herdar todos os metodos responsaveis por manipula do banco de dados (JPAREPOSITORY) 
+//proprio do Spring
+//metodo não padrão devem ser criados aqui
 @Repository
 public interface PostagemRepository extends JpaRepository <Postagem, Long> {
+	
+	// select * from tb_postagens where titulo like %titulo%;
+	//select = find
+	// * = All
+	//By = where
+	// Containing = like. Containing compativel com qualquer banco de dados, pode ser o like para MySQL
+	//IgnoreCase = não diferenciar maiusculo de minusculo
+	//String titulo = especifica a coluna
+	List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 
 }
